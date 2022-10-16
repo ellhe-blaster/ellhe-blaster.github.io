7df0db6ee14b672332f4f2245a7a0ccc3e297683
ellhe-blaster <danielprisco09@gmail.com>
2022-10-16 19:26:10 -0400

    Updates

---
+++ precache-manifest.ded9e2467c299380c447cbcc71441cf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0138769807739a23064e",
+    "url": "/static/css/main.a97ff0e6.chunk.css"
+  },
+  {
+    "revision": "0138769807739a23064e",
+    "url": "/static/js/main.5534e6c7.chunk.js"
+  },
+  {
+    "revision": "fae33ca72394ba16b1be",
+    "url": "/static/js/runtime~main.6cb09b41.js"
+  },
+  {
+    "revision": "ca8562f04f27eeb964b0",
+    "url": "/static/js/2.a77e7803.chunk.js"
+  },
+  {
+    "revision": "11d5b9d5be061bfd7000",
+    "url": "/static/js/3.479c0d5f.chunk.js"
+  },
+  {
+    "revision": "6eace61a737c047044af7f1448d4a8e2",
+    "url": "/index.html"
+  }
+];
